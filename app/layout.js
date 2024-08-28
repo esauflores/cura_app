db41e1c5a6248567a42b0d6e17e92e5d04b084a6
César Esaú Flores Martínez <esauflores2022@gmail.com>
2024-08-28 09:42:34 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
         />
         <meta
           name="viewport"
-          content="width=device-width; initial-scale=1; viewport-fit=cover"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
