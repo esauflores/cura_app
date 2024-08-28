0f852d46bb4880799aace6b82a37652b83337ef2
César Esaú Flores Martínez <esauflores2022@gmail.com>
2024-08-28 09:41:05 +0700

    Mobile safe bar

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,18 @@ export default function RootLayout({ children }) {
           href="/manifest.webmanifest"
           crossOrigin="use-credentials"
         />
+        <meta
+          name="viewport"
+          content="width=device-width; initial-scale=1; viewport-fit=cover"
+        />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
       </head>
-      <body>{children}</body>
+      <body class="h-safe-area-inset-top">{children}</body>
     </html>
   );
 }
